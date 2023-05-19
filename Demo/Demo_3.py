14ccf3f5b5391fd7f70791ba7a6536f758aacd09
Archie <110494298+archie122@users.noreply.github.com>
2023-05-19 16:51:37 -0600

    Needs more work

---
+++ Demo/Demo_3.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob
+from newspaper import Article
+
+url = 'https://en.wikipedia.org/wiki/Mathematics'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.text
+print(text)
