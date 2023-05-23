5c5b2b5b61421c472f6d519cd9eb4516be7dfd70
Archie <110494298+archie122@users.noreply.github.com>
2023-05-23 14:01:05 -0600

    Code wars practice

--- Demo/Demo_3.py
+++ Demo/Demo_3.py
@@ -1,12 +1,9 @@
-from textblob import TextBlob
-from newspaper import Article
+def to_camel_case(text):
+    words = text.replace('-', '_').split('_')
+    camel_case = words[0] + ''.join(word.title() for word in words[1:])
+    return camel_case
 
-url = 'https://en.wikipedia.org/wiki/Mathematics'
-article = Article(url)
-
-article.download()
-article.parse()
-article.nlp()
-
-text = article.text
-print(text)
+print(to_camel_case(""))
+print(to_camel_case("the-stealth-warrior"))
+print(to_camel_case("The_Stealth-Warrior"))
+print(to_camel_case("The_Stealth_Warrior"))
