4115b34f2235ef06a8c796bd85ed093b93849977
Archie <110494298+archie122@users.noreply.github.com>
2023-06-14 10:22:12 -0600

    Solved another

---
+++ Demo/Problem_28.py
@@ -0,0 +1,4 @@
+def xo(s):
+    return s.lower().count('x') == s.lower().count('o')
+
+print(xo('xoX'))
---
+++ Demo/Problem_29.py
@@ -0,0 +1,7 @@
+def eliminate_unset_bits(number):
+    if number.count('1') == 0:
+        return 0
+    return int(number.replace('0', ''), 2)
+
+
+print(eliminate_unset_bits("00000000000000000000000000001011"))
