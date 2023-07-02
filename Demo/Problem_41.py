95966879b99cc8cf77707484f3234c0ac2d5a951
Archie <110494298+archie122@users.noreply.github.com>
2023-07-01 20:13:24 -0600

    Update Problem_41.py

--- Demo/Problem_41.py
+++ Demo/Problem_41.py
@@ -6,6 +6,18 @@ def sum_array(a):
     return sum(a)
 
 
- 
+def gimme(input_array):
+    x = sorted(input_array)
+    middle = x[len(x) // 2]
+    return input_array.index(middle)
 
-print(gimme([1, 5, 7, 4, 2]))
+
+# print(gimme([1, 5, 7, 4, 2]))
+
+def merge(nums1, m, nums2, n):
+    for i in range(n):
+        nums1[m + i] = nums2[i]
+    nums1.sort()
+
+
+print(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
