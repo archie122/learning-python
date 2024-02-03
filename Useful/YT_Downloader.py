6cdda60f387019e74c321eacd9a2d9914fd19309
Archie <110494298+archie122@users.noreply.github.com>
2024-02-02 22:54:48 -0700

    Update YT_Downloader.py

--- Useful/YT_Downloader.py
+++ Useful/YT_Downloader.py
@@ -1,15 +1,42 @@
-from pytube import Playlist
+# from pytube import Playlist
 import ssl
+#
+# # Bypass SSL verification
+# ssl._create_default_https_context = ssl._create_unverified_context
+#
+# x = input("Enter Playlist URL: ")
+#
+# playlist = Playlist(x)
+# print("Total videos in playlist: ", len(playlist.video_urls))
+#
+# for video in playlist.videos:
+#     stream = video.streams.get_highest_resolution()
+#     print("Downloading: ", video.title)
+#     stream.download()
 
-# Bypass SSL verification
-ssl._create_default_https_context = ssl._create_unverified_context
 
-x = input("Enter Playlist URL: ")
+# importing packages
+from pytube import YouTube
+import os
 
-playlist = Playlist(x)
-print("Total videos in playlist: ", len(playlist.video_urls))
+# url input from user
+yt = YouTube(
+    str(input("Enter the URL of the video you want to download: \n>> ")))
 
-for video in playlist.videos:
-    stream = video.streams.get_highest_resolution()
-    print("Downloading: ", video.title)
-    stream.download()
+# extract only audio
+video = yt.streams.filter(only_audio=True).first()
+
+# check for destination to save file
+print("Enter the destination (leave blank for current directory)")
+destination = str(input(">> ")) or '.'
+
+# download the file
+out_file = video.download(output_path=destination)
+
+# save the file
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+
+# result of success
+print(yt.title + " has been successfully downloaded.")
